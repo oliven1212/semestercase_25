fb9c5d950c90694c1521d157b701bcab148794ec
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2025-10-30 09:35:56 +0100

    branch til branches

--- seeders/20251030074836-demo-branch.js
+++ seeders/20251030074836-demo-branch.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('User', [
+    await queryInterface.bulkInsert('Branches', [
       {
         name: 'OK Plus',
         createdAt: new Date(),
