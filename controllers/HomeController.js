fff2b06137d9db649413143b45b13021eb5775c9
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2025-10-30 10:52:00 +0100

    Update HomeController.js

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,9 +1,4 @@
-exports.index = ('/exports', (req, res) => {
-  res.render("home/index", {
-    title: "Welcome",
-    message: "Hello from MVC!",
-  });
-});
+
 exports.task = ('/task', (req, res) => {
   res.render("home/task", {
     title: "Welcome",
@@ -11,7 +6,7 @@ exports.task = ('/task', (req, res) => {
   });
 });
 
-exports.login = ('/login', (req, res) => {
+exports.login = ('/', (req, res) => {
   res.render("home/login", {
     title: 'login',
     message: 'skriv dit brugernavn og kodeord her'
