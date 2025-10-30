0fbd89e1ce5cd60fb2bb613364178329c4502009
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2025-10-30 09:20:27 +0100

    seeders
    seeders for users og branches

--- seeders/20251030074836-demo-branch.js
+++ seeders/20251030074836-demo-branch.js
@@ -2,19 +2,18 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('User', [
+      {
+        name: 'OK Plus',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
