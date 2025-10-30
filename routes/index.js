59474938cd19df735a7ba7ff987f6fb7371bbd48
nannaglaw1 <nannathedanishgirl@gmail.com>
2025-10-30 11:02:45 +0100

    router added

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const HomeController = require("../controllers/HomeController");
 
 const router = express.Router();
 
-router.get("/", HomeController.index);
 router.get("/task", HomeController.task);
-router.get('/login', HomeController.login)
+router.get('/', HomeController.login);
+router.get('/gasstation', HomeController.gasstation);
 
 module.exports = router;
