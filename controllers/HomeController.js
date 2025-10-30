7a4e6826a1a5113f9f27bb14ac33b420d9ed7c5f
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2025-10-30 10:48:47 +0100

    homeController rettet

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,18 +1,18 @@
-exports.index = (req, res) => {
+exports.index = ('/exports', (req, res) => {
   res.render("home/index", {
     title: "Welcome",
     message: "Hello from MVC!",
   });
-};
-exports.rengoring = (req, res) => {
-  res.render("home/rengoring", {
+});
+exports.task = ('/task', (req, res) => {
+  res.render("home/task", {
     title: "Welcome",
     message: "Hello from MVC!",
   });
-};
+});
 
-exports.login = ('/login',(req,res)=>{
-  res.render("home/login",{
+exports.login = ('/login', (req, res) => {
+  res.render("home/login", {
     title: 'login',
     message: 'skriv dit brugernavn og kodeord her'
   });
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const HomeController = require("../controllers/HomeController");
 const router = express.Router();
 
 router.get("/", HomeController.index);
-router.get("/rengoring", HomeController.rengoring);
+router.get("/task", HomeController.task);
 router.get('/login', HomeController.login)
 
 module.exports = router;
