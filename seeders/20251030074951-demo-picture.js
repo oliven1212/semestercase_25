654eeb80ec5aea4c15f6ff8ac34fdc0477f1c05b
Valdemar <pizza27@live.dk>
2025-10-30 09:49:13 +0100

    seeder product-task og picture

--- seeders/20251030074951-demo-picture.js
+++ seeders/20251030074951-demo-picture.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { v4: uuidv4 } = require('uuid');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -12,6 +14,25 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+
+      // Create an array of entries to insert
+      const picturesArray = [];
+
+      // Example: insert the same UUID twice using a loop
+      for (let i = 0; i < 100; i++) {
+          const pictureId = uuidv4();
+          picturesArray.push({
+              id: pictureId,              // same UUID for both
+              taskId: Math.floor(Math.random() * 100),
+              filename: pictureId,        // also the same filename
+              beforeAfter: Math.round(Math.random()),       // 0 or 1
+              productImage: Math.round(Math.random()),       // 0 or 1
+              createdAt: new Date(),
+              updatedAt: new Date(),
+          });
+      }
+    await queryInterface.bulkInsert('Pictures', picturesArray, {});
   },
 
   async down (queryInterface, Sequelize) {
