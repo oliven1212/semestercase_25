bf4589efe3ec454b679b8709a1ca7eb12fcfb193
nannaglaw1 <nannathedanishgirl@gmail.com>
2025-10-30 10:09:13 +0100

    task og gasstationUsers

--- seeders/20251030074930-demo-gasstation-user.js
+++ seeders/20251030074930-demo-gasstation-user.js
@@ -2,19 +2,29 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const GasstationUsersArray = [];
+
+    // Example: insert the same UUID twice using a loop
+    for (let i = 0; i < 50; i++) {
+      for (let y = 0; y < 20; y++) {
+
+        GasstationUsersArray.push({
+
+          userId: y,
+          gasstationId: i,
+          isOwner: Math.round(Math.random()),       // 0 or 1
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+      }
+    }
+    await queryInterface.bulkInsert('GasstationUsers', GasstationUsersArray, {});
+
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('GasstationUsers', null, {});
     /**
      * Add commands to revert seed here.
      *
