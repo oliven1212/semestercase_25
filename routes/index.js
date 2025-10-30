aa146c983531aa1c4202b53ad15a1edb13dd460e
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2025-10-30 13:42:22 +0100

    adminList index og filer

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const HomeController = require("../controllers/HomeController");
 const gasController = require("../controllers/gasController");
 const createTaskController = require("../controllers/createTaskController");
+const adminListController = require("../controllers/adminListController");
 
 const router = express.Router();
 
 router.get("/createTask", createTaskController.createTask);
 router.get('/', HomeController.login);
 router.get('/gasstation', gasController.gasstation);
+router.get('/adminList', adminListController.adminList);
 
 module.exports = router;
