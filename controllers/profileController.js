0ec6e8a1b248447a8e61cefbd008df1154411391
Valdemar <pizza27@live.dk>
2025-10-31 09:03:43 +0100

    ...

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,9 +1,10 @@
 const { User, Gasstation, GasstationUser} = require('../models');
 
 exports.profile = async (req, res) => {
-    const users = await User.findAll({ raw: true});
+    const users = await User.findAll();
+    const newUsers = users.map(user => user.toJSON());
     res.render("home/profile", {
         title: 'login',
-        users: users
+        users: newUsers
     });
 };
