50e2e679403dbeb04c90cd780361ce5f1c0ad7f3
Valdemar <pizza27@live.dk>
2025-10-31 09:20:12 +0100

    Example user

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,8 +3,10 @@ const { User, Gasstation, GasstationUser} = require('../models');
 exports.profile = async (req, res) => {
     const users = await User.findAll();
     const newUsers = users.map(user => user.toJSON());
+    const exampleUser = await User.findOne({ where: { id: 1 } });
     res.render("home/profile", {
         title: 'login',
-        users: newUsers
+        users: newUsers,
+        exampleUser: exampleUser.toJSON()
     });
 };
