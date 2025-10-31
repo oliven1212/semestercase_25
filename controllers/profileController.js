7ece53a66b6ebc11611b11a4dab9c1e366b1e3a1
Valdemar <pizza27@live.dk>
2025-10-31 13:26:05 +0100

    raw: true

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,16 +1,18 @@
 const { User, Gasstation, GasstationUser} = require('../models');
 
 exports.profile = async (req, res) => {
-    const currentUser = await User.findByPk(3);
+    const currentUser = await User.findByPk(3,{raw: true});
+console.log(currentUser);
 
-
-    const users = await User.findAll();
-    const newUsers = users.map(user => user.toJSON());
-    const exampleUser = await User.findOne({ where: { id: 1 } });
+    const users = await User.findAll({raw: true});
+    const exampleUser = await User.findOne({ 
+        where: { id: 1 },
+        raw: true 
+    });
     res.render("home/profile", {
         title: 'login',
-        users: newUsers,
-        exampleUser: exampleUser.toJSON(),
-        currentUser: currentUser.toJSON()
+        users: users,
+        exampleUser: exampleUser,
+        currentUser: currentUser
     });
 };
--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-        models.User.belongsToMany(models.Gasstation, { through: 'GasstationUsers' });
+        //models.User.belongsToMany(models.Gasstation, { through: 'GasstationUsers' });
     }
   }
   User.init({
