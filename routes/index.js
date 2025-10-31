a4fe3a58aae235bd9709b3bbdbc3fc67b28657f9
valde <fe13valseh@gmail.com>
2025-10-31 11:41:33 +0100

    made task history in controller route and hbs file in view

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,16 @@ const createTaskController = require("../controllers/createTaskController");
 const adminListController = require("../controllers/adminListController");
 const profileController = require("../controllers/profileController");
 const taskPageOneController = require("../controllers/taskPageOneController");
-
+const taskHistorieController = require("../controllers/taskHistorieController");
 
 const router = express.Router();
 
 router.get("/createTask", createTaskController.createTask);
-router.get('/', HomeController.login);
-router.get('/gasstation', gasController.gasstation);
-router.get('/adminList', adminListController.adminList);
-router.get('/profile', profileController.profile);
-router.get('/taskPageOne', taskPageOneController.taskPageOne);
+router.get("/", HomeController.login);
+router.get("/gasstation", gasController.gasstation);
+router.get("/adminList", adminListController.adminList);
+router.get("/profile", profileController.profile);
+router.get("/taskPageOne", taskPageOneController.taskPageOne);
+router.get("/taskHistorie", taskHistorieController.taskHistorie);
 
 module.exports = router;
