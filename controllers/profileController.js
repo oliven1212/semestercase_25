89d54e60ccd1e8c76a905f365b38b1de92652763
Valdemar <pizza27@live.dk>
2025-10-31 09:21:53 +0100

    svgdz

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,9 +4,11 @@ exports.profile = async (req, res) => {
     const users = await User.findAll();
     const newUsers = users.map(user => user.toJSON());
     const exampleUser = await User.findOne({ where: { id: 1 } });
+    const exampleUser2 = await User.findByPk(2);
     res.render("home/profile", {
         title: 'login',
         users: newUsers,
-        exampleUser: exampleUser.toJSON()
+        exampleUser: exampleUser.toJSON(),
+        exampleUser2: exampleUser2.toJSON()
     });
 };
