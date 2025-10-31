aac4f7223ff6e7ed562697b2c942cbd962e7e23e
nannaglaw1 <nannathedanishgirl@gmail.com>
2025-10-31 10:09:21 +0100

    gasstationId

--- controllers/gasController.js
+++ controllers/gasController.js
@@ -2,12 +2,23 @@ const { User, Gasstation, GasstationUser} = require('../models');
 
 exports.gasstation = async (req, res) => {
 
-    const users = await User.findAll({ raw: true});
+    const users = await User.findAll();
+    const owner = await User.findOne({ where: {id: 4}});
+    const gasstationId = await GasstationUser.findAll({ where: {userId: 4}});
+
+    console.log(gasstationId.join(', '));
+
+
+    const gasstations = await Gasstation.findAll({ where: {id: [(gasstationId.gasstationId.join(", "))]}});
+
+    console.log(gasstations);
 
   res.render("home/gasstation", {
     title: 'gasstation',
     message: 'Vælg tankstation',
     users: users,
-    gasstation: [{branch:'Cirkle K'},{branch:'OK'},{branch:'Uno-X'},{branch:'Q8'},{branch:'Shell'}]
+    owner: owner.toJSON(),
+    gasstations: gasstation,
   });
 };
+
