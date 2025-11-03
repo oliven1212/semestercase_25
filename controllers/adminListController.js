40cdac8fa3d4e30fbfdd78ea13ca210cedc24a69
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2025-11-03 09:23:28 +0100

    adminList updates + css doc

--- controllers/adminListController.js
+++ controllers/adminListController.js
@@ -1,9 +1,12 @@
-
-exports.adminList = (req, res) => {
-    res.render("home/gasstation", {
-        title: 'gasstation',
-        message: 'Vælg tankstation',
-        name: [{fornavn:'Steve'},{fornavn:'Lone'}, {fornavn:'Jan'}]
+const {User} = require("../models");
+exports.adminList = async (req, res) => {
+    const users = await User.findAll();
+    /*console.log(users[2].dataValues.firstName);
+    .dataValues virker ikke, men chatgpt har guidet os til at bruge { raw: true}*/
+    const newUsers = users.map(user => user.toJSON());
+    res.render("home/adminList", {
+        title: 'Liste af brugere',
+        message: 'Opret ny bruger',
+        name: newUsers
     });
-
 };
