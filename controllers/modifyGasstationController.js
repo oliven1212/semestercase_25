79295ac70f042352007bed1a9f79ae0762dd1cda
oliven1212 <60175607+oliven1212@users.noreply.github.com>
2025-11-16 01:32:31 +0100

    city added to gasstation update date, with fancy field

--- controllers/modifyGasstationController.js
+++ controllers/modifyGasstationController.js
@@ -1,20 +1,32 @@
+const { where } = require('sequelize');
 const { Gasstation, Branch, City } = require('../models');
 
 exports.modifyGasstation = async (req, res) => {
     const gasstation = await Gasstation.findAll({
-        //attributes: ['id', 'branchId', 'address', 'contactEmail', 'contactPhone', 'frontSpace'],
+        attributes: ['id', 'branchId', 'cityCode', 'address', 'contactEmail', 'contactPhone', 'frontSpace'],
         where: { id: req.params.id },
         include: [
             {
                 model: Branch,
                 attributes: ['name'] // only fetch the branch name
+            },
+            {
+                model: City,
+                attributes: ['name','zipCode']
             }
         ],
         raw: true,
     });
     //retreives all branches so you can pick the one you need
-    const branches = await Branch.findAll({ raw: true });
-    const cities = await City.findAll({raw:true});
+    const branches = await Branch.findAll({ 
+        attributes: ['id','name'],
+        raw: true 
+    });
+    const cities = await City.findAll({
+        attributes:['zipCode','name'],
+        raw:true
+
+    });
 console.log(cities);
     res.render("home/modifyGasstation", {
         title: 'login',
@@ -25,6 +37,22 @@ console.log(cities);
 };
 
 exports.updateGasstation = async (req, res) => {
+    let zipCode = req.body.city;
+    if(zipCode.lastIndexOf('(') == -1 || zipCode.lastIndexOf(')') == -1 || zipCode.lastIndexOf('(') > zipCode.lastIndexOf(')')){
+//Error handling format not correct: [city name] ([zip code])
+
+    }
+    zipCode = parseInt(zipCode.slice(zipCode.lastIndexOf('(') + 1, zipCode.lastIndexOf(')')));
+    const city = await City.findOne({
+        where:{zipCode: zipCode},
+        raw:true
+    });
+    if(!city){
+//Error handling city not found based on zip code
+
+    }
+
+
     await Gasstation.updateGasstation({
         id: req.params.id,
         branchId: req.body.branchId,
@@ -32,6 +60,8 @@ exports.updateGasstation = async (req, res) => {
         contactEmail: req.body.contactEmail,
         contactPhone: req.body.contactPhone,
         frontSpace: req.body.frontSpace,
+        cityCode: zipCode,
     });
+    
     res.redirect(`/modifyGasstation/${req.params.id}`);
 };
