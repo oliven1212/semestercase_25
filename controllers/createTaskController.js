cf36eec52c9dd6e6445f841606a55e1d4f126a3f
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2025-11-16 19:59:25 +0100

    oprettelse af task creation

--- controllers/createTaskController.js
+++ controllers/createTaskController.js
@@ -2,36 +2,60 @@
 const { User, Gasstation, Branch } = require('../models');
 
 exports.createTask = async (req, res) => {
-
-    const users = await User.findAll();
-    const gasstation = await Gasstation.findAll();
-    const branch = await Branch.findAll();
-
-
-    const gasstationIds = gasstationId.map(link => link.gasstationId);
+    try {
+        const users = await User.findOne({
+            where: { id: 3 },
+            raw: true
+        });
 
 
+        const gasstations = await Gasstation.findAll({
+            attributes: ['id', 'address'],
+            include: [
+                {
+                    model: Branch,
+                    attributes: ['name'], // kun branch name
+                }],
+            raw: true
+        }
+        );
 
-    const gasstations = await Gasstation.findAll({
-        attributes: ['id', 'branchId', 'address', 'contactEmail', 'contactPhone', 'frontSpace'],
-        where: { id: gasstationIds },
-        raw: true,
-        include: [
-            {
-                model: Branch,
 
-                as: 'branch',
-                attributes: ['name'] // only fetch the branch name
-            }
-        ],
-    });
-    console.log(gasstations);
+        const branchOrder = ['Q8', 'Shell', 'OK Plus', 'UNO-X', 'Cirkle K'];
 
-    res.render("home/gasstation", {
-        title: 'gasstation',
-        message: 'Vælg tankstation',
-        users: users,
-        owner: owner.toJSON(),
-        gasstations: gasstations,
-    });
+        const branchDropdowns = branchOrder.map((branchName) => {
+            return {
+                name: branchName,
+                stations: gasstations
+                    .filter(gs => gs.Branch && gs.Branch.name === branchName)
+                    .map(gs => ({
+                        id: gs.id,
+                        address: gs.address
+                    }))
+            };
+        });
+
+        res.render('home/createTask', {
+            title: 'velkommen',
+            message: 'Vælg tankstation',
+            users: users,
+            gasstation: gasstations,
+            branchDropdowns
+        });
+
+
+
+
+
+    } catch (err) {
+        console.error('Fejl i createTask:');
+        console.error('Sequelize name:', err.name);
+        console.error('Sequelize message:', err.message);
+        if (err.original) {
+            console.error('MySQL message:', err.original.message);
+            console.error('MySQL code:', err.original.code);
+            console.error('MySQL sql:', err.original.sql);
+        }
+        res.status(500).send('Der opstod en fejl på serveren');
+    }
 };
