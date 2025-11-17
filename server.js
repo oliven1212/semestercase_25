9111af95184bcfd050900dc3c6b0894015a5864b
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2025-11-17 12:53:27 +0100

    host club

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+  console.log(`Server running on http://91.98.73.200/:${PORT}`);
 });
 
