db83c38617ed16c2fea35a4ac9c8dd34954798ce
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2025-11-17 12:48:32 +0100

    task fcuk up

--- controllers/adminListController.js
+++ controllers/adminListController.js
@@ -1,43 +1,44 @@
 const {User, Gasstation, City, Product} = require("../models");
-const {all} = require("express/lib/application");
 
 exports.adminListUsers = async (req, res) => {
     const users = await User.findAll({
-        order: [['firstName', 'ASC']]
+        order: [['lastName', 'ASC']],
+        raw: true
     });
     /*console.log(users[2].dataValues.firstName);
     .dataValues virker ikke, men chatgpt har guidet os til at bruge { raw: true}*/
-    const allUsers = users.map(user => user.toJSON());
     res.render("home/adminList", {
         title: 'Liste af brugere',
         message: 'Opret ny bruger',
-        name: allUsers.lastName + allUsers.firstName
+        users: users,
+        name: `${users.lastName}, ${users.firstName}`
     });
 };
 
 exports.adminListGasstations = async (req, res) => {
     const stations = await Gasstation.findAll({
+        order: [['address', 'ASC']]
     });
     /*console.log(users[2].dataValues.firstName);
     .dataValues virker ikke, men chatgpt har guidet os til at bruge { raw: true}*/
     const allstations = stations.map(station => station.toJSON());
     res.render("home/adminList", {
-        title: 'Liste af brugere',
-        message: 'Opret ny bruger',
+        title: 'Liste af tankstationer',
+        message: 'Opret ny tankstation',
         name: allstations
     });
 };
 
 exports.adminListProducts = async (req, res) => {
     const products = await Product.findAll({
-        order: [['Adress', 'ASC']]
+        order: [['name', 'ASC']]
     });
     /*console.log(users[2].dataValues.firstName);
     .dataValues virker ikke, men chatgpt har guidet os til at bruge { raw: true}*/
     const allProducts = products.map(product => product.toJSON());
     res.render("home/adminList", {
-        title: 'Liste af brugere',
-        message: 'Opret ny bruger',
+        title: 'Liste af produkter',
+        message: 'Opret nyt product',
         name: allProducts
     });
 };
