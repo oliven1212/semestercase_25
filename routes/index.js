d0b372f6245f5e4a95b54c6d4f7f470e2dd13f9f
nannaglaw1 <nannathedanishgirl@gmail.com>
2025-11-17 13:28:06 +0100

    task page one --> createTaskData

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const gasController = require("../controllers/gasController");
 const createTaskController = require("../controllers/createTaskController");
 const adminListController = require("../controllers/adminListController");
 const profileController = require("../controllers/profileController");
-const taskPageOneController = require("../controllers/taskPageOneController");
+const createTaskDataController = require("../controllers/createTaskDataController");
 const taskHistorieController = require("../controllers/taskHistorieController");
 const modifyGasstationController = require("../controllers/modifyGasstationController");
 
@@ -17,7 +17,7 @@ router.get("/adminList/users", adminListController.adminListUsers);
 router.get("/adminList/gasstations", adminListController.adminListGasstations);
 router.get("/adminList/products", adminListController.adminListProducts);
 router.get("/profile", profileController.profile);
-router.get("/taskPageOne", taskPageOneController.taskPageOne);
+router.get("/createtaskdata/:userId", createTaskDataController.taskPageOne);
 router.get("/taskHistorie", taskHistorieController.taskHistorie);
 
 router.get("/admin/gasstation/:id", modifyGasstationController.adminGasstation);
