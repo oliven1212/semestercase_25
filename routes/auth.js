c3127a7c0193d310b4ca79da9aaa543b8998a5a3
valde <fe13valseh@gmail.com>
2025-11-26 13:08:53 +0100

    den opretter en session nu ihvertfald der mangler en del

--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,45 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const { isNotAuthenticated } = require("../utility/auth");
-//get
+//get auth login
+router.get("/login", isNotAuthenticated, (res, req) => {
+  res.render("login", {
+    error: req.session.error,
+  });
+});
+
+//Post/auth/login
+router.post("/login", isNotAuthenticated, (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validering
+    if (!email || !password) {
+      req.session.error = "Email og adgangskode skal udfyldes;";
+      return res.render("/login");
+    }
+    //find bruger
+    const user = users.find((u) => u.email === email);
+    if (!user) {
+      req.session.error = "forkert email eller adgangskode";
+      return res.render("/login");
+    }
+    //verificere adgangskode
+    const isValidPassword = await;
+    bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      req.session.error = "forkert email eller adgangskode";
+      return res.redirect("/login");
+    }
+    //gem bruger data i session (uden adgangskode)
+    req.session.user = {
+      id: user.id,
+      userName: user.firstname + user.lastname,
+      email: user.mail,
+    };
+    res.redirect("/createTask");
+  } catch (error) {
+    console.error("login fejl:", error);
+    req.session.error = "der opstod en fejl prøv igen.";
+    res.redirect("/login");
+  }
+});
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const app = require("./app");
-const HOST = process.env.HOST || '0.0.0.0';
+const session = require("express-session");
+const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+app.use(
+  session({
+    secret: "12345",
+    cookie: {
+      secure: "false",
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  }),
+);
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  console.log(req.session);
+  next();
+});
