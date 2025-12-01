526bbd1b99a22818df60191e4e4f937197665126
Valdemar <pizza27@live.dk>
2025-12-01 07:52:59 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,6 @@
-<<<<<<< Updated upstream
 "use strict";
 const bcrypt = require("bcrypt");
 const { Model } = require("sequelize");
-=======
-'use strict';
-const {
-    Model
-} = require('sequelize');
-
->>>>>>> Stashed changes
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static async getAllUserIds() {
@@ -19,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
       return allUsers.map((user) => user.id);
     }
 
-<<<<<<< Updated upstream
     static async updateUser({
       id,
       firstName,
@@ -98,23 +89,3 @@ module.exports = (sequelize, DataTypes) => {
   });
   return User;
 };
-=======
-    User.init({
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        roleId: DataTypes.INTEGER,
-        password: DataTypes.STRING,
-        phone: DataTypes.STRING,
-        address: DataTypes.STRING
-    }, {
-        sequelize,
-        modelName: 'User',
-        defaultScope: {
-            attributes: { exclude: ['password'] },
-        }
-    });
-    
-    return User;
-};
->>>>>>> Stashed changes
