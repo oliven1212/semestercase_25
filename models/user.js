89b6b76c5685f1c4d4ddbe4b1777b251d82092b5
Signeamadsen97 <signe.amalie@live.dk>
2025-12-01 07:07:37 +0100

    rettelser

--- models/user.js
+++ models/user.js
@@ -8,6 +8,9 @@ const {
     Model
 } = require('sequelize');
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
