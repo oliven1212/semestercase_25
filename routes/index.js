fbcdda540a706034dad9ac8e33c5b6d30ebbf1aa
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2025-12-02 12:56:06 +0100

    routes lidt bedre opsætning

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,6 @@ router.get("/createTask", createTaskController.createTask);
 router.get("/", HomeController.login);
 router.post("/login", HomeController.loginSend);
 router.get("/gasstation", gasController.gasstation);
-router.get("/admin/users", adminListController.adminListUsers);
-router.get("/admin/gasstations", adminListController.adminListGasstations);
-router.get("/admin/products", adminListController.adminListProducts);
 router.get("/profile", profileController.profile);
 router.get("/createtaskdata/:taskId", createTaskDataController.taskPageOne);
 router.get("/taskHistorie", taskHistorieController.taskHistorie);
@@ -35,11 +32,13 @@ router.get("/completedTask/:taskId", createTaskDataController.completedTask);
 
 router.post("/createTask", createTaskController.logStart);
 
+router.get("/admin/gasstations", adminListController.adminListGasstations);
 router.get("/admin/gasstations/:gasId", modifyGasstationController.adminGasstation,);
 router.post("/admin/gasstations/new", adminListController.newGasstationStart);
 router.post("/admin/gasstations/:gasId/update", modifyGasstationController.updateGasstation,);
 router.post("/admin/gasstations/:gasId/delete", modifyGasstationController.deleteGasstation,);
 
+router.get("/admin/users", adminListController.adminListUsers);
 router.get("/admin/users/:userId", profileController.adminUser);
 router.post("/admin/users/new", adminListController.newUserStart);
 router.post("/admin/users/:userId/update", profileController.updateUser);
@@ -47,11 +46,13 @@ router.post("/admin/users/:userId/delete", profileController.deleteUser);
 router.get("/admin/users/:userId/tasks", profileController.tasks);
 router.get("/admin/users/:userId/gasstations", profileController.gasstations);
 
+router.get("/admin/products", adminListController.adminListProducts);
 router.get("/admin/products/:productId", productController.adminProducts);
 router.post("/admin/products/new", adminListController.newProductStart);
 router.post("/admin/products/:productId/update", productController.updateProduct,);
 router.post("/admin/products/:productId/delete", productController.deleteProduct,);
 
+//router.get("/admin/tasks", adminListController.adminListTasks);
 //router.get("/admin/tasks/:taskId", taskController);
 //router.post("/admin/tasks/:taskId/update", taskController);
 router.post("/admin/tasks/:taskId/delete", taskController.deleteTask);
