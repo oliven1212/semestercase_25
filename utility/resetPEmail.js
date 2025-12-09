deb0ccf2ad320679af528c50721d9a50ab5c5f67
Signeamadsen97 <signe.amalie@live.dk>
2025-12-09 13:12:24 +0100

    tekst rettelse email

--- utility/resetPEmail.js
+++ utility/resetPEmail.js
@@ -18,12 +18,10 @@ if (!GMAIL_USER || !GMAIL_PASS) {
 
 async function resetPasswordEmail(imageUuid, toEmail) {
     const html = `<h3>Reset dit password</h3>
-        <p>Hej ejer af tankstation</p>
-        <p>Der er netop blevet uploadet en ny rengøringsopgave for din tankstation.</p>
-        <p>Du kan se billederne og oplysningerne ved at klikke på linket nedenfor:</p>
+        <p>Følg linket nednfor:</p>
         <br>
         <a href="http://localhost:3000/showTaskImages/${imageUuid}">Se billeder for opgaven her</a>
-        <p>Dette link er gyldigt i 48 timer og kan kun bruges en gang.</p>
+        <p>Dette link er gyldigt i 1 time og kan kun bruges en gang.</p>
         <p>Dette er en automatisk genereret email, svar venligst ikke på denne.</p>
         `;
 
@@ -41,7 +39,7 @@ async function resetPasswordEmail(imageUuid, toEmail) {
     const mailOptions = {
         from: `"Automatisk email" <${GMAIL_USER}>`,
         to: toEmail, //Put din egen email her for at teste
-        subject: 'Ny rengøringsopgave uploadet',
+        subject: 'NReset pasword',
         html: html,
     };
 
