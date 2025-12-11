314a1b5f77b3f3c407fc56e1f17c9ec7e618e65b
JonatanEmil <FoxyOnyxSheep@Gmail.com>
2025-12-11 11:40:12 +0100

    uuid code til reset password

--- models/user.js
+++ models/user.js
@@ -77,6 +77,9 @@ module.exports = (sequelize, DataTypes) => {
       password: DataTypes.STRING,
       phone: DataTypes.STRING,
       address: DataTypes.STRING,
+        passwordCodeUuid: DataTypes.UUID,
+        passwordExpired: DataTypes.DATE,
+
     },
     {
       sequelize,
