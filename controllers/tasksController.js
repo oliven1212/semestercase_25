2ed2128732f12a68588b6793982150dc2abf3a57
Valdemar <pizza27@live.dk>
2025-12-11 11:03:25 +0100

    ...

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,34 +1,45 @@
-const { Task, User, Gasstation, Picture, Branch } = require('../models');
+const { Task, User, Gasstation, Picture, Branch, Product, ProductTask } = require('../models');
+const path = require('path');
+const fs = require('fs');
+const { where } = require('sequelize');
 
 exports.adminTasks = async (req, res) => {
     const task = await Task.findOne({
         where: { id: req.params.taskId },
-        attributes: ['startTime'],
+        attributes: ['id', 'startTime'],
         include: [{
             model: User,
-            attributes: ['id','firstName','lastName'],
+            attributes: ['id', 'firstName', 'lastName'],
         },
         {
             model: Gasstation,
-            attributes: ['id', 'address','frontSpace'],
+            attributes: ['id', 'address', 'frontSpace'],
             include: [{
                 model: Branch,
             }],
         }],
         raw: true,
     });
+    const tzOffset = task.startTime.getTimezoneOffset() * 60000; // in ms
+    task.startTime = new Date(task.startTime.getTime() - tzOffset).toISOString().slice(0, 16);
+
     const pictures = await Task.findAll({
         where: { id: req.params.taskId },
         attributes: [],
         include: [
-        {
-            model: Picture,
-        }],
+            {
+                model: Picture,
+            }],
+        raw: true,
+    });
+
+    const products = await ProductTask.findAll({
+        where: {taskId: req.params.taskId},
         raw: true,
     });
 
     const users = await User.findAll({
-        attributes: ['id','firstName','lastName'],
+        attributes: ['id', 'firstName', 'lastName'],
         raw: true,
     });
     const gasstations = await Gasstation.findAll({
@@ -40,8 +51,7 @@ exports.adminTasks = async (req, res) => {
         raw: true,
     });
 
-    console.log(pictures);
-
+    console.log(products);
     res.render("admin/modifyTask", {
         task: task,
         pictures: pictures,
@@ -61,12 +71,21 @@ exports.deleteTask = async (req, res) => {
     await Task.destroy({
         where: { id: req.params.taskId, },
     });
-    res.redirect(`${req.body.link}`);
+    res.redirect(`/admin/tasks`);
 };
 
-exports.deleteImage = async (req, res) =>{
+exports.deleteImage = async (req, res) => {
+    //deletes Picture db entry
     await Picture.destroy({
         where: { fileName: req.body.fileName, },
     });
-    res.redirect(`${req.body.link}`);
+    //deletes image file
+    fs.unlink(path.join(__dirname, '..', 'public', 'imgUploads', req.body.fileName), (err) => {
+        if (err) {
+            console.error('Fejl ved sletning af fil:', err);
+        } else {
+            console.log('Fil slettet succesfuldt');
+        }
+    });
+    res.redirect(`/admin/tasks/${req.params.taskId}`);
 };
