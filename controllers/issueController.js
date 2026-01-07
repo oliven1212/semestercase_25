699d659ccd84f077b2b3e9b0300bdc3b31c1905f
valde <114578370+HerbForest@users.noreply.github.com>
2026-01-07 20:22:45 +0100

    issue routes
    issure routes

---
+++ controllers/issueController.js
@@ -0,0 +1,1 @@
+const { User, Issue, Task } = require("../models");
--- routes/index.js
+++ routes/index.js
@@ -18,63 +18,216 @@ const router = express.Router();
 
 //Universelle routes
 router.get("/", loginController.login); //Loginside
-router.get("/profile", allowRoles([1,2,3]), profileController.profile); //Viser profil baseret på id
-
+router.get("/profile", allowRoles([1, 2, 3]), profileController.profile); //Viser profil baseret på id
 
 //Personale routes
-router.get("/createTask", allowRoles([1,3]), createTaskController.createTask); //Start på task
-router.post("/createTask", allowRoles([1,3]), createTaskController.logStart); //Send id med videre
-router.get("/createtaskdata/:taskId", allowRoles([1,3]), createTaskDataController.taskPageOne); //Tilføj billeder og/eller produktopfyldningner
-router.post("/uploadtaskimage/:taskId", allowRoles([1,3]), createTaskDataController.uploadMiddleware, createTaskDataController.imageUpload); //Tilføj til Pictures table
-router.get("/createtaskdata/:taskId/images", allowRoles([1,3]), createTaskDataController.viewImages); //Se uploadede billeder
-router.post("/createtaskdata/:taskId/images", allowRoles([1,3]), createTaskDataController.deleteImage,); //Slet uploadede billeder
-router.post("/uploadtask/:taskId", allowRoles([1,3]), createTaskDataController.uploadTasks); //Tilføj til ProductTask table og send mail til ejer
-router.get("/completedtask/:taskId", allowRoles([1,3]), createTaskDataController.completedTask); //Færdig med task upload
-router.get("/tasks", allowRoles([1,3]), taskController.userListTasks); //Færdig med task upload
-router.get("/tasks/:taskId", allowRoles([1,3]), taskController.userTasks); //Færdig med task upload
+router.get("/createTask", allowRoles([1, 3]), createTaskController.createTask); //Start på task
+router.post("/createTask", allowRoles([1, 3]), createTaskController.logStart); //Send id med videre
+router.get(
+  "/createtaskdata/:taskId",
+  allowRoles([1, 3]),
+  createTaskDataController.taskPageOne,
+); //Tilføj billeder og/eller produktopfyldningner
+router.post(
+  "/uploadtaskimage/:taskId",
+  allowRoles([1, 3]),
+  createTaskDataController.uploadMiddleware,
+  createTaskDataController.imageUpload,
+); //Tilføj til Pictures table
+router.get(
+  "/createtaskdata/:taskId/images",
+  allowRoles([1, 3]),
+  createTaskDataController.viewImages,
+); //Se uploadede billeder
+router.post(
+  "/createtaskdata/:taskId/images",
+  allowRoles([1, 3]),
+  createTaskDataController.deleteImage,
+); //Slet uploadede billeder
+router.post(
+  "/uploadtask/:taskId",
+  allowRoles([1, 3]),
+  createTaskDataController.uploadTasks,
+); //Tilføj til ProductTask table og send mail til ejer
+router.get(
+  "/completedtask/:taskId",
+  allowRoles([1, 3]),
+  createTaskDataController.completedTask,
+); //Færdig med task upload
+router.get("/tasks", allowRoles([1, 3]), taskController.userListTasks); //Færdig med task upload
+router.get("/tasks/:taskId", allowRoles([1, 3]), taskController.userTasks); //Færdig med task upload
 
 //Stationsejer routes
-router.get("/gasstation", allowRoles([1,2]), gasController.gasstation); //Ejer's tankstationer
-router.get("/taskHistorie/:taskId", allowRoles([1,2]), taskHistorieController.taskHistorie); //Tasks på den valgte tankstation
-router.get("/showTaskImages/:imageUuid", createTaskDataController.showTaskImages); //Side som mail til ejer fører til
+router.get("/gasstation", allowRoles([1, 2]), gasController.gasstation); //Ejer's tankstationer
+router.get(
+  "/taskHistorie/:taskId",
+  allowRoles([1, 2]),
+  taskHistorieController.taskHistorie,
+); //Tasks på den valgte tankstation
+router.get(
+  "/showTaskImages/:imageUuid",
+  createTaskDataController.showTaskImages,
+); //Side som mail til ejer fører til
 
-
-//Admin routes
+//--------------ADMIN-GASTATION---------------
 router.get("/admin", allowRoles([1]), adminListController.adminMain);
 
-router.get("/admin/gasstations", allowRoles([1]), adminListController.adminListGasstations);
-router.get("/admin/gasstations/:gasId", allowRoles([1,2]), modifyGasstationController.adminGasstation,);
-router.post("/admin/gasstations/new", allowRoles([1]), adminListController.newGasstationStart);
-router.post("/admin/gasstations/:gasId/update", allowRoles([1]), modifyGasstationController.updateGasstation,);
-router.post("/admin/gasstations/:gasId/delete", allowRoles([1]), modifyGasstationController.deleteGasstation,);
-router.get("/admin/gasstations/:gasId/tasks", allowRoles([1,2]), modifyGasstationController.tasks);
-router.get("/admin/gasstations/:gasId/users", allowRoles([1,2]), modifyGasstationController.users);
-router.post("/admin/gasstations/:gasId/users/new", allowRoles([1]), modifyGasstationController.createLinkUser);
-router.post("/admin/gasstations/:gasId/users/:userId/delete", allowRoles([1]), modifyGasstationController.removeLinkUser);
-
-
+router.get(
+  "/admin/gasstations",
+  allowRoles([1]),
+  adminListController.adminListGasstations,
+);
+router.get(
+  "/admin/gasstations/:gasId",
+  allowRoles([1, 2]),
+  modifyGasstationController.adminGasstation,
+);
+router.post(
+  "/admin/gasstations/new",
+  allowRoles([1]),
+  adminListController.newGasstationStart,
+);
+router.post(
+  "/admin/gasstations/:gasId/update",
+  allowRoles([1]),
+  modifyGasstationController.updateGasstation,
+);
+router.post(
+  "/admin/gasstations/:gasId/delete",
+  allowRoles([1]),
+  modifyGasstationController.deleteGasstation,
+);
+router.get(
+  "/admin/gasstations/:gasId/tasks",
+  allowRoles([1, 2]),
+  modifyGasstationController.tasks,
+);
+router.get(
+  "/admin/gasstations/:gasId/users",
+  allowRoles([1, 2]),
+  modifyGasstationController.users,
+);
+router.post(
+  "/admin/gasstations/:gasId/users/new",
+  allowRoles([1]),
+  modifyGasstationController.createLinkUser,
+);
+router.post(
+  "/admin/gasstations/:gasId/users/:userId/delete",
+  allowRoles([1]),
+  modifyGasstationController.removeLinkUser,
+);
+//-----------ADMIN-USER---------------
 router.get("/admin/users", allowRoles([1]), adminListController.adminListUsers);
-router.get("/admin/users/:userId", allowRoles([1]), profileController.adminUser);
-router.post("/admin/users/new", allowRoles([1]), adminListController.newUserStart);
-router.post("/admin/users/:userId/update", allowRoles([1]), profileController.updateUser);
-router.post("/admin/users/:userId/delete", allowRoles([1]), profileController.deleteUser);
-router.get("/admin/users/:userId/tasks", allowRoles([1]), profileController.tasks);
-router.get("/admin/users/:userId/gasstations", allowRoles([1]), profileController.gasstations);
-router.post("/admin/users/:userId/gasstations/new", allowRoles([1]), profileController.linkGasstation);
-router.post("/admin/users/:userId/gasstations/:gasId/delete", allowRoles([1]), profileController.removeLinkGasstation);
-
-
-router.get("/admin/products", allowRoles([1]), adminListController.adminListProducts);
-router.get("/admin/products/:productId", allowRoles([1]), productController.adminProducts);
-router.post("/admin/products/new", allowRoles([1]), adminListController.newProductStart);
-router.post("/admin/products/:productId/update", allowRoles([1]), productController.updateProduct);
-router.post("/admin/products/:productId/delete", allowRoles([1]), productController.deleteProduct);
-
+router.get(
+  "/admin/users/:userId",
+  allowRoles([1]),
+  profileController.adminUser,
+);
+router.post(
+  "/admin/users/new",
+  allowRoles([1]),
+  adminListController.newUserStart,
+);
+router.post(
+  "/admin/users/:userId/update",
+  allowRoles([1]),
+  profileController.updateUser,
+);
+router.post(
+  "/admin/users/:userId/delete",
+  allowRoles([1]),
+  profileController.deleteUser,
+);
+router.get(
+  "/admin/users/:userId/tasks",
+  allowRoles([1]),
+  profileController.tasks,
+);
+router.get(
+  "/admin/users/:userId/gasstations",
+  allowRoles([1]),
+  profileController.gasstations,
+);
+router.post(
+  "/admin/users/:userId/gasstations/new",
+  allowRoles([1]),
+  profileController.linkGasstation,
+);
+router.post(
+  "/admin/users/:userId/gasstations/:gasId/delete",
+  allowRoles([1]),
+  profileController.removeLinkGasstation,
+);
+//----------------ADMIN-PRODUCTS---------------
+router.get(
+  "/admin/products",
+  allowRoles([1]),
+  adminListController.adminListProducts,
+);
+router.get(
+  "/admin/products/:productId",
+  allowRoles([1]),
+  productController.adminProducts,
+);
+router.post(
+  "/admin/products/new",
+  allowRoles([1]),
+  adminListController.newProductStart,
+);
+router.post(
+  "/admin/products/:productId/update",
+  allowRoles([1]),
+  productController.updateProduct,
+);
+router.post(
+  "/admin/products/:productId/delete",
+  allowRoles([1]),
+  productController.deleteProduct,
+);
+//----------------ADMIN-TASKS---------------
 router.get("/admin/tasks", allowRoles([1]), adminListController.adminListTasks);
-router.get("/admin/tasks/:taskId", allowRoles([1,2]), taskController.adminTasks);
-router.post("/admin/tasks/:taskId/update", allowRoles([1]), taskController.updateTask);
-router.post("/admin/tasks/:taskId/delete", allowRoles([1]), taskController.deleteTask);
-router.post("/admin/tasks/:taskId/delete/image", allowRoles([1]), taskController.deleteImage);
+router.get(
+  "/admin/tasks/:taskId",
+  allowRoles([1, 2]),
+  taskController.adminTasks,
+);
+router.post(
+  "/admin/tasks/:taskId/update",
+  allowRoles([1]),
+  taskController.updateTask,
+);
+router.post(
+  "/admin/tasks/:taskId/delete",
+  allowRoles([1]),
+  taskController.deleteTask,
+);
+router.post(
+  "/admin/tasks/:taskId/delete/image",
+  allowRoles([1]),
+  taskController.deleteImage,
+);
+//----------------ISSUE---------------
+router.get(
+  "/admin/tasks/:taskId/issueHistory",
+  allowRoles([1]),
+  issueController.issueTaskHistory,
+);
+router.get(
+  "/admin/tasks/:taskId/:issueId",
+  allowRoles([1]),
+  issueController.issueTaskHistory,
+);
+router.delete(
+  "/admin/tasks/:taskId/issue",
+  allowRoles([1]),
+  issueController.issueDelete,
+);
+router.post(
+  "/admin/tasks/:taskId/issue",
+  allowRoles([1, 2, 3]),
+  issueController.issueCreate,
+);
 
 router.use("/", authRoutes);
 module.exports = router;
