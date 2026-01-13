fcb525474896bb2f1114b3209560519ae8399e14
Signeamadsen97 <signe.amalie@live.dk>
2026-01-13 14:45:06 +0100

    relation mellem issue og task

--- models/issue.js
+++ models/issue.js
@@ -9,10 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
-  }
+  static associate(models) {
+  Issue.belongsTo(models.Task, {
+    foreignKey: "taskId",
+    onDelete: "CASCADE"
+  });
+}
   Issue.init({
     taskId: DataTypes.INTEGER,
     status: DataTypes.BOOLEAN,
