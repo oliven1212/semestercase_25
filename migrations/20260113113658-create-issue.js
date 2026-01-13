6b86cd26ae20246de50de38f9db7d4c14c135811
Signeamadsen97 <signe.amalie@live.dk>
2026-01-13 12:38:26 +0100

    indsat migration data i create issue

--- migrations/20260113113658-create-issue.js
+++ migrations/20260113113658-create-issue.js
@@ -1,22 +1,38 @@
-'use strict';
-
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Issues", {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      taskId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Tasks",
+          key: "id",
+        },
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+      },
+      comment: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Issues");
+  },
 };
